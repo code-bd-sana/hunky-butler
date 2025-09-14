4702b0d3fb09e5f14d7df95b5ae01174488cdc42
KhalidTheCoder <khalidahsan2407@gmail.com>
2025-09-14 17:57:19 +0600

    cleared page.js

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,9 @@
-import Navbar from "@/components/shared/Navbar";
-import ServiceBanner from "@/components/shared/ServiceBanner";
 import React from "react";
 
 export default function page() {
   return (
     <div className="min-h-screen bg-white">
-      <ServiceBanner></ServiceBanner>
-      <Navbar></Navbar>
+      <h1>Hello World</h1>
     </div>
   );
 }
