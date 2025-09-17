8da8b40fb05071af227e0fcc24063ef3522caccb
AnkonChy <ankonchy1@gmail.com>
2025-09-17 18:19:39 +0600

    footer section changed,height added in frequently asked question section in home page

--- frontend/app/contact/page.js
+++ frontend/app/contact/page.js
@@ -2,6 +2,9 @@
 import Banner from "@/components/shared/Banner";
 import img from "../../public/Blog/BlogSection/BlogSection9.jpeg";
 import ContactForm from "@/components/contact/ContactForm";
+import Card from "@/components/Dashboard/Card";
+import Planning from "@/components/homepage/Planning";
+import Footer from "@/components/homepage/Footer";
 const contact = () => {
   return (
     <div className="bg-[#f6f4f5] min-h-screen">
@@ -13,7 +16,10 @@ const contact = () => {
           "Have questions or ready to book? Our team is here to help with quotes, custom requests, and support. Reach out anytime and let us bring energy, style, and fun to your event."
         }
       />
+      {/* <Card /> */}
       <ContactForm />
+      <Planning/>
+      <Footer/>
     </div>
   );
 };
