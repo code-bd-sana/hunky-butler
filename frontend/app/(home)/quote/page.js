36ef26834063ef05bf88ac089ef73855838feebf
rakib3719 <bannah76769@gmail.com>
2025-09-20 14:55:08 +0600

    add qoute section

---
+++ frontend/app/(home)/quote/page.js
@@ -0,0 +1,13 @@
+import FirstStep from '@/components/quote/FirstStep'
+import React from 'react'
+
+export default function page() {
+  return (
+    <div className='min-h-screen'>
+
+      <FirstStep/>
+      
+      
+      </div>
+  )
+}
