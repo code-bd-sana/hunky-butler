ca32b089bf128a9c27b34ef35d9987faf58f1acf
KhalidTheCoder <khalidahsan2407@gmail.com>
2025-09-20 15:17:51 +0600

    service page added for dashboard

---
+++ frontend/app/(dashboard)/dashboard/services/page.js
@@ -0,0 +1,10 @@
+import ServiceTable from "@/components/Dashboard/ServicesTable/ServiceTable";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <ServiceTable></ServiceTable>
+    </div>
+  );
+}
