541df77f53ccfe41befc01a9cc5cd031ee7d77c4
rakib3719 <bannah76769@gmail.com>
2025-09-21 09:44:59 +0600

    add type moudle in pacakge.json

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 
 app.use(cors({
-    origin: ['http://localhost:3000',  'http://localhost:5174', 'http://localhost:5173','http://localhost:3001', 'http://localhost:3002' ],
+    origin: ['http://localhost:3000',  'http://localhost:5174', 'http://localhost:5173','http://localhost:3001', 'http://localhost:3002', "https://hunky-butler.vercel.app" ],
     credentials: true 
 }));
 
