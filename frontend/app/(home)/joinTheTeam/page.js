2ca3ae2e0834dc4cb92be9d966d198b7e4e81671
KhalidTheCoder <khalidahsan2407@gmail.com>
2025-10-08 17:56:22 +0600

    join page all design fixed also added workwithus section

--- frontend/app/(home)/joinTheTeam/page.js
+++ frontend/app/(home)/joinTheTeam/page.js
@@ -2,6 +2,7 @@ import Footer from "@/components/homepage/Footer";
 import img from "@/public/ImageGalary/pic2.jpeg";
 import JoinForm from "@/components/JoinTheTeam/JoinForm";
 import ServAndAboutBanner from "@/components/shared/typography/ServAndAboutBanner";
+import WorkWithUsSection from "@/components/JoinTheTeam/WorkWithUsSection";
 
 export default function joinTheTeam() {
   return (
@@ -13,6 +14,7 @@ export default function joinTheTeam() {
         description={"Looking for flexible, fun, and well-paid work? Hunky Butler Service is always on the lookout for confident, professional, and outgoing people to join our team. From buff butlers and topless waiters to cocktail masterclass hosts and strippers, we provide some of the best party entertainment jobs in the UK."}
       />
       <JoinForm />
+      <WorkWithUsSection/>
       <Footer />
     </div>
   );
