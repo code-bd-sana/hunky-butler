f25e3122f69b624ab18e73b8ef6c0c24b0cbcf75
AnkonChy <ankonchy1@gmail.com>
2025-10-11 13:04:13 +0600

    socket url changed in socket.js

--- frontend/utils/socket.js
+++ frontend/utils/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-// const socket = io("https://hunky-butler-backend.vercel.app/api");
-const socket = io("http://localhost:5000");
+const socket = io("https://hunky-butler-backend.vercel.app/api");
+// const socket = io("http://localhost:5000");
 export default socket;
