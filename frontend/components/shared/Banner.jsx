69fdfe9cf9076490a9512122df83d3a24abbab04
AnkonChy <ankonchy1@gmail.com>
2025-11-11 15:27:56 +0600

    locantion page updated succesfully

--- frontend/components/shared/Banner.jsx
+++ frontend/components/shared/Banner.jsx
@@ -2,14 +2,15 @@ import Image from "next/image";
 import React from "react";
 import arrow from "@/public/icons/greaterthan.png";
 import img from "@/public/Blog/BlogSection/BlogSection9.jpeg";
-export default function Banner({serviceName, service, title, description, image, banner }) {
+import Link from "next/link";
+export default function Banner({ service, title, description, image }) {
   console.log(image);
   return (
     <div
       style={{
-        backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 13%, rgba(0,0,0,1) 95%), url(${image?.src})`,
+        backgroundImage: `linear-gradient(180deg, rgba(0,0,0,0) 13%, rgba(0,0,0,1) 95%), url(${image})`,
       }}
-      className="relative h-[585px] w-full overflow-hidden bg-cover bg-center"
+      className="relative h-[620px] w-full overflow-hidden bg-cover bg-center"
     >
       {/* Main Content */}
       <div className="relative z-10 flex h-full flex-col items-center justify-end pb-10 text-center">
@@ -24,16 +25,20 @@ export default function Banner({serviceName, service, title, description, image,
             height={10}
           />
           <p className="tracking-[-1px]">{service}</p>
-          
         </div>
 
-        <h1 className="mb-2 max-w-7xl text-4xl capitalize tracking-[-1px] font-medium text-white md:text-5xl lg:text-[60px]">
+        <h1 className="mb-2 max-w-7xl text-3xl capitalize tracking-[-1px] font-medium text-white md:text-5xl lg:text-[60px]">
           {title}
         </h1>
 
-        <p className="mb-8 mt-4 max-w-7xl text-lg text-white md:text-xl">
+        <p className="mb-8 mt-4 max-w-7xl text-xs md:text-base lg:text-lg text-white">
           {description}
         </p>
+        <Link href={`/quote`}>
+          <button className="rounded-full border-2 border-white px-4 py-2 md:px-[24px] md:py-[16px] text-lg font-semibold bg-white text-[#292929]">
+            Call +44 7745 865352
+          </button>
+        </Link>
       </div>
     </div>
   );
