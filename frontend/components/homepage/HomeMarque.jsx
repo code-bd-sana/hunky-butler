510c37fa45c969762a5cf106bd20835ab33885fe
rakib3719 <rakib3719>
2026-02-03 09:53:37 +0600

    faw

--- frontend/components/homepage/HomeMarque.jsx
+++ frontend/components/homepage/HomeMarque.jsx
@@ -1,114 +1,149 @@
-import React from 'react'
-import image1 from '@/public/icons/home/frame.svg'
-import image2 from '@/public/icons/home/m2.png'
-import Image from 'next/image'
-import { Marquee } from '../magicui/marquee'
+import image1 from "@/public/icons/home/frame.svg";
+import image2 from "@/public/icons/home/m2.png";
+import Image from "next/image";
+import { Marquee } from "../magicui/marquee";
 
 export default function HomeMarque() {
   return (
     <div className='md:mt-8 mx-auto overflow-hidden min-h-72 md:min-h-96'>
-        
+      <section className='mt-16 mx-auto'>
+        <div className='flex rotate-2 bg-[#FF66A6] items-center gap-5'>
+          <Marquee reverse>
+            <Image
+              alt='icon'
+              src={image1}
+              className='h-[25px]  w-[25px] mt-4'
+            />
+            <p className='text-[#F6F4F5]  py-4  pr-4 capitalize'>
+              Play Party games
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 '>Take Photos</p>
+            <Image
+              alt='icon'
+              src={image2}
+              className='h-[25px] w-[25px] capitalize mt-4'
+            />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize '>
+              Professional
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Service with a Smile
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Nationwide Coverage
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Easy Booking</p>
+            <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Play Party games
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Take Photos</p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Professional</p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Service with a Smile
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Nationwide Coverage
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Easy Booking</p>
+            <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Play Party games
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Take Photos</p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize '>
+              Professional
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Service with a Smile
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Nationwide Coverage
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Easy Booking</p>
+          </Marquee>
+        </div>
+      </section>
+      <section className='mt-4 mx-auto'>
+        <div className='flex  rotate-2 bg-[#FF006A] items-center gap-5'>
+          <Marquee>
+            <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Ultimate Party Hosts
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Best in the UK
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>fun & classy</p>
 
-<section className='mt-16 mx-auto'>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Life Drawing</p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>buff butlers</p>
 
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              male Strippers
+            </p>
+            <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Ultimate Party Hosts
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Best in the UK
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>fun & classy</p>
 
-    <div className='flex rotate-2 bg-[#FF66A6] items-center gap-5'>
-       <Marquee reverse>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Life Drawing</p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>buff butlers</p>
 
-             <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
-        <p className='text-[#F6F4F5]  py-4 pr-4'>Play  Party games</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Take Photos</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Professional</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Service with a Smile</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Nationwide Coverage</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Easy Booking</p>
-             <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
-        <p className='text-[#F6F4F5]  py-4 pr-4'>Play  Party games</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Take Photos</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Professional</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Service with a Smile</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Nationwide Coverage</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Easy Booking</p>
-             <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
-        <p className='text-[#F6F4F5]  py-4 pr-4'>Play  Party games</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Take Photos</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Professional</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Service with a Smile</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Nationwide Coverage</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Easy Booking</p>
-   
-       
-       </Marquee>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              male Strippers
+            </p>
+            <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Ultimate Party Hosts
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              Best in the UK
+            </p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>fun & classy</p>
+
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>Life Drawing</p>
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>buff butlers</p>
+
+            <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4' />
+            <p className='text-[#F6F4F5]  py-4 pr-4 capitalize'>
+              male Strippers
+            </p>
+          </Marquee>
+        </div>
+      </section>
     </div>
-</section>
-<section className='mt-4 mx-auto'>
-
-
-    <div className='flex  rotate-2 bg-[#FF006A] items-center gap-5'>
-      <Marquee >
-
-          <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
-        <p className='text-[#F6F4F5]  py-4 pr-4'>Ultimate Party Hosts</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Best in the UK</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>fun & classy</p>
-       
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Life Drawing</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>buff butlers</p>
-       
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>male Strippers</p>
-          <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
-        <p className='text-[#F6F4F5]  py-4 pr-4'>Ultimate Party Hosts</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Best in the UK</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>fun & classy</p>
-       
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Life Drawing</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>buff butlers</p>
-       
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>male Strippers</p>
-          <Image alt='icon' src={image1} className='h-[25px] w-[25px] mt-4' />
-        <p className='text-[#F6F4F5]  py-4 pr-4'>Ultimate Party Hosts</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Best in the UK</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>fun & classy</p>
-       
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>Life Drawing</p>
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>buff butlers</p>
-       
-        <Image alt='icon' src={image2} className='h-[25px] w-[25px] mt-4'/>
-        <p className='text-[#F6F4F5]  py-4 pr-4 '>male Strippers</p>
-       
-      </Marquee>
-    </div>
-</section>
-
-    </div>
-  )
+  );
 }
--- frontend/components/homepage/Planning.jsx
+++ frontend/components/homepage/Planning.jsx
@@ -1,54 +1,54 @@
-import React from "react";
+"use client";
+import { useGetBlogsQuery } from "@/features/blogApi";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 const Planning = ({ title, highlight, description }) => {
-  const posts = [
-    {
-      id: 1,
-      date: "February 11, 2014",
-      title: "Cocktail Making: Tools You Need At Home",
-      desc: "From Buff Butlers to Masterclasses—transparent pricing, verified staff, anywhere in your postcode.",
-      img: "/Planning/planningPic1.jpeg",
-    },
-    {
-      id: 2,
-      date: "October 24, 2018",
-      title: "7 Hen Party Ideas Tools That Always Work",
-      desc: "From Buff Butlers to Masterclasses—transparent pricing, verified staff, anywhere in your postcode.",
-      img: "/Planning/planningPic2.jpeg",
-    },
-    {
-      id: 3,
-      date: "May 31, 2015",
-      title: "Why Dynamic Pricing Keeps Costs Fair",
-      desc: "From Buff Butlers to Masterclasses—transparent pricing, verified staff, anywhere in your postcode.",
-      img: "/Planning/planningPic3.jpeg",
-    },
-  ];
+  const { data = [] } = useGetBlogsQuery();
+  const router = useRouter();
+
+  // ✅ only first 3 active blogs
+  const posts = data.slice(0, 3);
+
   return (
-    <section className="max-w-[1240px] mx-auto px-4 py-4 md:py-12 pb-[500px]">
-      <div className="text-center mb-6 md:mb-20 space-y-6">
-        <h1 className="text-2xl md:text-5xl font-semibold">
+    <section className='max-w-[1240px] mx-auto px-4 py-4 md:py-12 pb-[500px]'>
+      <div className='text-center mb-6 md:mb-20 space-y-6'>
+        <h1 className='text-2xl md:text-5xl font-semibold'>
           {title}
-          <span className="text-[#FF006A] italic">{highlight}</span>
+          <span className='text-[#FF006A] italic'>{highlight}</span>
         </h1>
-        <p className="text-base md:text-lg md:w-3/5 mx-auto">{description}</p>
+        <p className='text-base md:text-lg md:w-3/5 mx-auto'>{description}</p>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-6">
+
+      <div className='grid grid-cols-1 md:grid-cols-3 gap-3 md:gap-6'>
         {posts.map((post) => (
-          <div key={post.id} className="rounded-2xl overflow-hidden">
+          <div
+            key={post._id}
+            onClick={() => router.push(`/blog/${post.slug}`)}
+            className='rounded-2xl cursor-pointer overflow-hidden'>
             <Image
-              src={post.img}
-              alt="Hen party planning inspiration with buff butlers"
-              title=" Hen Party Ideas Blog UK"
+              src={post.thumbnailUrl}
+              alt={post.title}
+              title={post.title}
               width={396}
               height={372}
-              className="w-full h-64 object-cover rounded-lg"
+              className='w-full h-64 object-cover rounded-lg'
             />
-            <div className="mt-4">
-              <p className="text-pink-600 text-sm font-medium">{post.date}</p>
-              <h3 className="text-2xl font-semibold mt-2">{post.title}</h3>
-              <p className="text-[#808080] text-base mt-2">{post.desc}</p>
+
+            <div className='mt-4'>
+              <p className='text-pink-600 text-sm font-medium'>
+                {new Date(post.date || post.createdAt).toLocaleDateString(
+                  "en-GB",
+                  { day: "numeric", month: "long", year: "numeric" },
+                )}
+              </p>
+
+              <h3 className='text-2xl font-semibold mt-2'>{post.title}</h3>
+
+              <p className='text-[#808080] text-base mt-2 line-clamp-2'>
+                {/* short preview from content */}
+                {post.content?.replace(/<[^>]*>/g, "")?.slice(0, 120)}...
+              </p>
             </div>
           </div>
         ))}
