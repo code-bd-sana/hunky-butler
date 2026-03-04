24f49419e79a6b7caed881274831ee7ef4c407f7
Syed Rakib Hasan <128777840+rakib3719@users.noreply.github.com>
2026-03-04 08:23:56 +0600

    Update CORS origin for hunkybutlerservice

--- backend/app.js
+++ backend/app.js
@@ -27,7 +27,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: ['http://localhost:5174', 'http://localhost:5173', 'http://localhost:3000', 'https://hnk-test.vercel.app', "https://hunky-butler.vercel.app"],
-    origin: "https://hunkybutlerservice.co.uk",
+    origin: ['https://www.hunkybutlerservice.co.uk', 'https://hunkybutlerservice.co.uk', 'https://www.hunkybutlerservice.co.uk/', 'https://hunkybutlerservice.co.uk/'],
     credentials: true,
   },
   transports: ["websocket", "polling"],
