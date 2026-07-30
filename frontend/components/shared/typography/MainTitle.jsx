fa579dc95482e981c84be3559bd107d29ea0d759
hunkybutlerservice <william.mckeown@icloud.com>
2026-07-30 14:58:45 +0100

    Add 'as' prop to MainTitle for dynamic headingsAdd optional as prop to MainTitle for heading-level control

--- frontend/components/shared/typography/MainTitle.jsx
+++ frontend/components/shared/typography/MainTitle.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
-export default function MainTitle({text}) {
+export default function MainTitle({text, as = 'h4'}) {
+  const Heading = as;
   return (
   
 
 
         <div>
-            <h4 className='text-[#141414] text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium leading-snug  max-w-7xl mx-auto  '>
+            <Heading className='text-[#141414] text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-medium leading-snug  max-w-7xl mx-auto  '>
 
           {text}
-        </h4>
+        </Heading>
         </div>
     
   )
